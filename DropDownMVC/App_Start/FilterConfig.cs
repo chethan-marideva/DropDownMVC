8c7be06d7a76188dd6f97d26d1c67a55b394ebe2
Chethan M <chethan.m2@dxc.com>
2018-06-18 12:02:15 +0530

    Add project files.

---
+++ DropDownMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DropDownMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DropDownMVC.MvcApplication" Language="C#" %>
